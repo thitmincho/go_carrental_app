45dffa1c9129f652c60593c88a41518ae37938f4
thitmincho <thitmincho@gmail.com>
2020-12-16 01:32:24 +0630

    Source Add

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  imgurl:any;
+  constructor() {
+    
+  }
+  ngOnInit() {
+    this.imgurl = "https://cars.usnews.com/pics/size/640x420/static/images/article/202006/128503/216702_New_Volvo_XC40_-_exterior_640x420.jpg";
+  }
+
+}
